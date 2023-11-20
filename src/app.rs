bddf62437e9b23c481733a56e95826f73f88d07e
Gavin Daly <gavdaly@protonmail.com>
2023-11-20 02:05:50 -0500

    fix: send a message, not just a pin number

--- src/app.rs
+++ src/app.rs
@@ -266,7 +266,9 @@ async fn submit_phone_number(phone: String) -> Result<(), ServerFnError> {
         return Err(ServerFnError::ServerError("Error Creating Pin!".into()));
     };
 
-    send_message(pin.number.to_string(), format!("+1{phone}")).await;
+    let pin_number = pin.number.to_string();
+    let message = format!("Your verification code is: {pin_number}. Do not share it.");
+    send_message(message, format!("+1{phone}")).await;
 
     leptos_axum::redirect(&("/p/".to_string() + &phone));
 
